864a5328068fbc013960993f4154f05e63b47ac3
Sevn <benjamin.castromadrid28et32@gmail.com>
2025-04-27 14:57:10 -0300

    Finalización Primer Bloque
    Realización del primer Bloque de la Guía

--- guia_Ejercicios_1/src/main/java/bloque_System/main.java
+++ guia_Ejercicios_1/src/main/java/bloque_System/main.java
@@ -1,10 +1,43 @@
 package bloque_System;
 
-public class main {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-	public static void main(String[] args) {
-		ejercicios first_block = new ejercicios();
-		first_block.primer_Ejercicio();
-	}
-
+public class main{
+	public static void main(String[] args){
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Seleccione una opción entre 1-8.");
+		while(true) {
+			try {
+				System.out.print("Seleccione que Ejercicio quiere ejecutar: ");
+				int aux = Integer.parseInt(br.readLine());
+					switch(aux){
+					case 1:
+						Ejercicios.primer_Ejercicio();
+						break;
+					case 2:
+						Ejercicios.segundo_Ejercicio();
+						break;
+					case 3:
+						Ejercicios.tercer_Ejercicio();
+						break;
+					case 4:
+						Ejercicios.cuarto_Ejercicio();
+						break;
+					case 5:
+						Ejercicios.quinto_Ejercicio();
+						break;
+					case 7:
+						Ejercicios.septimo_Ejercicio();
+						break;
+					default:
+						System.out.println("Seleccione una opción entre 1-8.");
+						break;
+					}
+			}catch(IOException e){
+				System.out.println(e);
+			}
+		}
+		}
 }
