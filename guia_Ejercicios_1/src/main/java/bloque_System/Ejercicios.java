03f99aa42307e1e3f2d98da1048f9d04b75da546
Sevn <benjamin.castromadrid28et32@gmail.com>
2025-04-29 09:48:29 -0300

    Correción Errores
    Se corrigen Errores en el primer Bloque

--- guia_Ejercicios_1/src/main/java/bloque_System/Ejercicios.java
+++ guia_Ejercicios_1/src/main/java/bloque_System/Ejercicios.java
@@ -9,14 +9,28 @@ import java.util.Locale;
 public class Ejercicios {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static PrintStream ps = new PrintStream(System.out);
+	
+	
+	
+	public static String leerLinea() throws IOException{
+		int aux;
+		String palabra = "";
+		while( (aux = System.in.read()) != '\n' ){
+			if (aux != '\r') {
+	            palabra = palabra + (char) aux;
+	        }
+		}
+		return palabra;
+	}
+	
 	public static void primer_Ejercicio(){//Metodo de clase, Exclusivo de la misma
 		try {
 			ps.print("Ingrese el valor por hora: ");
-			String txt = br.readLine();
+			String txt = leerLinea();
 			int valorHora = Integer.parseInt(txt);
 			ps.println("Valor de sus horas: " + valorHora);
 			ps.print("Ingrese el valor de horas trabajadas: ");
-			txt = br.readLine();
+			txt = leerLinea();
 			int horaTrabajo = Integer.parseInt(txt);
 			ps.println("Sus horas trabajadas: " + horaTrabajo);
 			int sueldoBruto = valorHora * horaTrabajo;
@@ -27,11 +41,11 @@ public class Ejercicios {
 	public static void segundo_Ejercicio(){
 		try {
 			ps.print("Ingrese su primer ángulo: ");
-			int ang1 = Integer.parseInt(br.readLine());
+			int ang1 = Integer.parseInt(leerLinea());
 			ps.println("Su ángulo es de: " + ang1);
 			//
 			ps.print("Ingrese su segundo ángulo: ");
-			int ang2 = Integer.parseInt(br.readLine());
+			int ang2 = Integer.parseInt(leerLinea());
 			ps.println("Su ángulo es de: " + ang2);
 			int total;
 			if((ang1 + ang2) < 180){
@@ -46,7 +60,7 @@ public class Ejercicios {
 	public static void tercer_Ejercicio(){
 		try {
 			ps.print("Ingrese la superficie del cuadrado en metros cuadrados: ");
-			int sup = Integer.parseInt(br.readLine());
+			int sup = Integer.parseInt(leerLinea());
 			double lado = Math.sqrt(sup);
 			int perimeter = (int) (4*lado);
 			ps.printf("El perimetro del valor ingresado es de: %d", perimeter);
@@ -55,7 +69,7 @@ public class Ejercicios {
 	public static void cuarto_Ejercicio(){
 		try {
 			ps.print("Ingrese su temperatura en Fahrenheits: ");
-			int tempF = Integer.parseInt(br.readLine());
+			int tempF = Integer.parseInt(leerLinea());
 			int tempC = (tempF-32) * 5/9;
 			ps.println("Su temperatura en Celsius es de: "+ tempC);
 		}catch(IOException e){}
@@ -63,7 +77,7 @@ public class Ejercicios {
 	public static void quinto_Ejercicio(){
 		ps.println("Ingrese su valor en segundos: ");
 		try {
-			int sec = Integer.parseInt(br.readLine());
+			int sec = Integer.parseInt(leerLinea());
 			if (sec < 0) {
 				ps.println("Por favor ingrese un valor positivo para los segundos.");
 	            return;
@@ -81,9 +95,9 @@ public class Ejercicios {
 	public static void sexto_Ejercicio(){
 		try {
 			ps.print("Ingrese el valor del producto: ");
-			int valor = Integer.parseInt(br.readLine());
+			int valor = Integer.parseInt(leerLinea());
 			ps.print("Ingrese que plan desea aplicar: ");
-			int plan = Integer.parseInt(br.readLine());
+			int plan = Integer.parseInt(leerLinea());
 			switch(plan) {
 			case 1:
 				int desc = (int)(valor * 0.10);
@@ -110,7 +124,7 @@ public class Ejercicios {
 	public static void septimo_Ejercicio(){
 		ps.println("Ingrese su signo zodiacal: ");
 			try {
-				String signo = br.readLine();
+				String signo = leerLinea();
 				signo = signo.toLowerCase();
 					switch(signo){
 					case "aries":
