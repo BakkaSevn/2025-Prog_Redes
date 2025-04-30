d078147aeec07a811719fb2bc508730ac2608811
Sevn <benjamin.castromadrid28et32@gmail.com>
2025-04-29 23:54:58 -0300

    Finalización UsandoReader
    Se termina la clase UsandoReader para la fecha de entrega.

--- guia_Ejercicios_1/src/main/java/bloque_System/UsandoSystem.java
+++ guia_Ejercicios_1/src/main/java/bloque_System/UsandoSystem.java
@@ -168,4 +168,3 @@ public class UsandoSystem {
 				        catch(IOException e){}
 					}
 			}
-
--- guia_Ejercicios_1/src/main/java/bloque_System/main.java
+++ guia_Ejercicios_1/src/main/java/bloque_System/main.java
@@ -3,42 +3,80 @@ package bloque_System;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintStream;
 
 public class main{
 	public static void main(String[] args){
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		System.out.println("Seleccione una opción entre 1-8.");
+		PrintStream err = new PrintStream(System.err);
 		while(true) {
+			System.out.println("Seleccione una opción entre 1 y 2.");
 			try {
-				System.out.print("Seleccione que Ejercicio quiere ejecutar: ");
+				System.out.print("Seleccione que Bloque quiere usar: ");
 				int aux = Integer.parseInt(br.readLine());
-					switch(aux){
+				switch(aux){
+				case 1:
+					System.out.println("Usted eligió el bloque de Usando System class");
+					System.out.println("Seleccione una opción entre 1 y 8.");
+					System.out.print("Seleccione que Ejercicio quiere ejecutar: ");
+					int aux2 = Integer.parseInt(br.readLine());
+					switch(aux2) {
+						case 1:
+							UsandoSystem.primer_Ejercicio();
+							break;
+						case 2:
+							UsandoSystem.segundo_Ejercicio();
+							break;
+						case 3:
+							UsandoSystem.tercer_Ejercicio();
+							break;
+						case 4:
+							UsandoSystem.cuarto_Ejercicio();
+							break;
+						case 5:
+							UsandoSystem.quinto_Ejercicio();
+							break;
+						case 7:
+							UsandoSystem.septimo_Ejercicio();
+							break;
+						default:
+							return;
+						}
+				case 2:
+					System.out.println("Usted eligió el bloque de Usando Reader class");
+					System.out.println("Seleccione una opción entre 1 y 8.");
+					System.out.print("Seleccione que Ejercicio quiere ejecutar: ");
+					int aux3 = Integer.parseInt(br.readLine());
+					switch(aux3) {
 					case 1:
-						UsandoSystem.primer_Ejercicio();
+						UsandoReader.primer_ejercicio();
 						break;
 					case 2:
-						UsandoSystem.segundo_Ejercicio();
+						UsandoReader.segundo_ejercicio();
 						break;
 					case 3:
-						UsandoSystem.tercer_Ejercicio();
+						UsandoReader.tercer_ejercicio();
 						break;
 					case 4:
-						UsandoSystem.cuarto_Ejercicio();
+						UsandoReader.cuarto_ejercicio();
 						break;
 					case 5:
-						UsandoSystem.quinto_Ejercicio();
+						UsandoReader.quinto_ejercicio();
+						break;
+					case 6:
+						UsandoReader.sexto_ejercicio();
 						break;
 					case 7:
-						UsandoSystem.septimo_Ejercicio();
+						UsandoReader.septimo_ejercicio();
 						break;
 					case 8:
-						UsandoReader.segundo_ejercicio();
-					default:
-						System.out.println("Seleccione una opción entre 1-8.");
+						UsandoReader.octavo_ejercicio();
 						break;
+						}
 					}
-			}catch(IOException e){
-				System.out.println(e);
+				}		
+			catch(IOException | NumberFormatException e){
+				err.println("Datos ingresados en el menú fueron incorrectos");
 			}
 		}
 		}
